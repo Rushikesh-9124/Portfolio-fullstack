00e82616cd9e7a69df7ee6c348858f69e39a63d6
Rushikesh-9124 <rushikesh6281@gmail.com>
2026-04-16 18:20:37 +0530

    Update api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env. || 'https://portfolio-fullstack-4afv.onrender.com/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://portfolio-fullstack-4afv.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
